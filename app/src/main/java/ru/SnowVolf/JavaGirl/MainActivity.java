23f8154ae4bd177002fc0cf3592e18a9e921389f
SnowVolf <aftervolf@gmail.com>
2016-12-08 22:47:32 +0700

    1.0.12

--- app/src/main/java/ru/SnowVolf/JavaGirl/MainActivity.java
+++ app/src/main/java/ru/SnowVolf/JavaGirl/MainActivity.java
@@ -1,8 +1,12 @@
 package ru.SnowVolf.JavaGirl;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Process;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +21,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.io.BufferedReader;
@@ -31,11 +36,11 @@ public class MainActivity extends AppCompatActivity {
         //toolbar
         Toolbar Toolbar=(Toolbar) findViewById(R.id.ToolBar);
         setSupportActionBar(Toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);//если true - показывается стрелочка на месте hamburger
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_open_nav_drawer);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//если true - показывается стрелочка на месте hamburger
         //getSupportActionBar().setIcon(R.mipmap.ic_launcher);//задает иконку в тулбаре. выглядит некрасиво, но кому-то может быть сгодится
-        getSupportActionBar().setTitle((Html.fromHtml("<font color=\"#ffffff\">"+getString(R.string.app_name)+ "</font>")));
+        getSupportActionBar().setTitle((Html.fromHtml("<font color=\"#ffffff\">"+getString(R.string.app_name)+ " Dev" +"</font>")));
         getSupportActionBar().setSubtitle((Html.fromHtml("<font color=\"#ffffff\">"+getString(R.string.created_by)+ "</font>")));
-
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -63,18 +68,30 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);
     }
+    //собираем инфу о приложении
     public void showAboutDialog(){
         new MaterialDialog.Builder(this)
-                .title(R.string.about_program)
-                .content("Автор:\nSnow Volf (Артём Жиганов)\nLicenced under the MIT LICENSE\n\n\nДата компиляции: 06.12.2016 21:37")
-                .positiveText("Понятно")
+                .title(getBuildName(this))
+                .content(R.string.about_content)
+                .positiveText(R.string.cle_ar)
                 .positiveColorRes(R.color.colorAccent)
                 .show();
     }
+    public static String getBuildName(Context context){
+        String programBuild = context.getString(R.string.app_name);
+        try{
+            String pkg = context.getPackageName();
+            PackageInfo pkgInfo = context.getPackageManager().getPackageInfo(pkg, PackageManager.GET_META_DATA);
+            programBuild += " v."+pkgInfo.versionName+" build "+pkgInfo.versionCode;
+
+
+        }catch (PackageManager.NameNotFoundException e1){
+        }return programBuild;
+    }
     public void onGirlClick(View v){
         String[] rnd = new String[]{
-                "Привет","JavaGirl v2","Powered by Snow Volf","Image Provided by NeoArtCore","ТЕСТ","ТЕСТ2","ТЕСТ3",
-                "ТЕСТ4","ТЕСТ5","ТЕСТ6","ТЕСТ7","ТЕСТ8","ТЕСТ9","ТЕСТ10"
+                getString(R.string.hello),getString(R.string.jgv2),getString(R.string.powered_me),getString(R.string.image_provided),getString(R.string.test_1),getString(R.string.test_2),getString(R.string.test_3),
+                getString(R.string.test_4),getString(R.string.test_5),getString(R.string.test_6),getString(R.string.test_7),getString(R.string.test_8),getString(R.string.test_9),getString(R.string.test_10)
         };
         Toast toast = Toast.makeText(getApplicationContext(), rnd[(int) (Math.random()*rnd.length)], Toast.LENGTH_SHORT);
         toast.show();
@@ -85,12 +102,15 @@ public class MainActivity extends AppCompatActivity {
             girlImageView.setImageResource(R.mipmap.ic_launcher);
             toastContainer.addView(girlImageView,1);
         }catch (Exception VolfLog){/*Toast.makeText(getApplicationContext(),"Исключение обработано",Toast.LENGTH_LONG).show();*/
-            Snackbar.make(findViewById(R.id.activity_main), "Исключение обработано",Snackbar.LENGTH_LONG).show();}
+           Snackbar.make(findViewById(R.id.activity_main), "Исключение обработано", Snackbar.LENGTH_LONG)
+                   .setDuration(2500)//2,5 секунды
+                   .show();
+        }
         toast.show();
     }
     public void onDeviantArtClick(View view){
-        Toast toast = Toast.makeText(getApplicationContext(), "Скоро...", Toast.LENGTH_LONG);
-        toast.show();
+        Snackbar.make(findViewById(R.id.myGirl), R.string.coming_soon, Snackbar.LENGTH_SHORT)
+        .show();
     }
     public void onWebClick(View view){
         Uri uri = Uri.parse("http://4pda.ru/forum/index.php?showuser=4324432");
@@ -98,21 +118,29 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
     private void showChangelog() {
-        StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
         try {
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(getAssets().open("changelog.txt"), "UTF-8"));
+            BufferedReader br = new BufferedReader(new InputStreamReader(getAssets().open("change.log"), "UTF-8"));
             String line;
             while ((line = br.readLine()) != null) {
                 sb.append(line).append("\n");
             }
-
         } catch (IOException e) {
         }
-       new MaterialDialog.Builder(this)
-                .title("JavaGirl v.1.0.8 build 9")
+        final String clToast = "<b><font color=\"#c62828\">Powered by Snow Volf</font><p><font color=\"#1565c0\"><i>А это текст другого цвета с курсивом</i></font></b><p>A вот это обычный не текст";
+        new MaterialDialog.Builder(this)
+                .title(getBuildName(this))
                 .content(sb)
-                .positiveText("Ok")
+                .positiveText(R.string.ok)
+                .neutralText("Debug")
+                .onNeutral(new MaterialDialog.SingleButtonCallback(){
+                    @Override
+                    public void onClick(@NonNull MaterialDialog materialDialog, @NonNull DialogAction dialogAction){
+                        Toast tst = Toast.makeText(getApplicationContext(),Html.fromHtml(clToast), Toast.LENGTH_LONG);
+                        tst.show();
+                    }
+                })
                 .show();
     }
     public void onBackPressed(){
---
+++ app/src/test/java/ru/SnowVolf/JavaGirl/MainActivityTest.java
@@ -0,0 +1,56 @@
+package ru.SnowVolf.JavaGirl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Snow Volf on 07.12.2016.
+ */
+public class MainActivityTest {
+    @Test
+    public void onOptionsItemSelected() throws Exception {
+
+    }
+
+    @Test
+    public void goToGit() throws Exception {
+
+    }
+
+    @Test
+    public void showAboutDialog() throws Exception {
+
+    }
+
+    @Test
+    public void getBuildName() throws Exception {
+
+    }
+
+    @Test
+    public void onGirlClick() throws Exception {
+
+    }
+
+    @Test
+    public void onDeviantArtClick() throws Exception {
+
+    }
+
+    @Test
+    public void onWebClick() throws Exception {
+
+    }
+
+    @Test
+    public void onBackPressed() throws Exception {
+
+    }
+
+    @Test
+    public void onExitClick() throws Exception {
+
+    }
+
+}
